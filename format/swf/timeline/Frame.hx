00d0891deb4e9c4e4091cdcc88b19b4b429d7d7f
Benjamin Davis <its.hip@gmail.com>
2014-02-17 10:33:49 +0000

    Only reference PlaceObjectTag if something changed

--- format/swf/timeline/Frame.hx
+++ format/swf/timeline/Frame.hx
@@ -67,8 +67,9 @@ class Frame
 				// A character id is defined:
 				// This means that the previous character is replaced 
 				// (possible transforms defined in previous frames are discarded)
-				frameObject.lastModifiedAtIndex = 0;
-				frameObject.placedAtIndex = tagIndex;
+				if(tag.hasName || tag.hasMatrix || tag.hasColorTransform || tag.hasFilterList) {
+					frameObject.lastModifiedAtIndex = tagIndex;
+				}
 				frameObject.isKeyframe = true;
 				if(tag.characterId != frameObject.characterId) {
 					// The character id does not match the previous character:
